0ffd64e544533db460d2b081228f0e6509b4b57e
Sean Harrison <sharrison9@gmail.com>
2024-02-14 16:00:22 -0600

    comments added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,10 @@
-console.log('***** Music Collection *****')
-// Safe Zone -- Write code below this line
+console.log('***** Music Collection *****');
 
+// Define an array to store the music collection
 let myCollection = [];
 
-function addToCollection(collection, title, artist, yearPublished){
+// Function to add an album to the collection
+function addToCollection(collection, title, artist, yearPublished) {
   let album = {
     title: title,
     artist: artist,
@@ -13,6 +14,7 @@ function addToCollection(collection, title, artist, yearPublished){
   return album;
 }
 
+// Add albums to the collection
 console.log("Adding albums to my collection.");
 console.log(addToCollection(myCollection, 'Laurel Hell', 'Mitski', 2022));
 console.log(addToCollection(myCollection, 'Titanic Rising', 'Weyes Blood', 2019));
@@ -22,32 +24,34 @@ console.log(addToCollection(myCollection, 'Blouse', 'Blouse', 2011));
 console.log(addToCollection(myCollection, 'Pet Sounds', 'The Beach Boys', 1966));
 console.log("My collection:", myCollection);
 
-function showCollection(collection){
-  for(let i =0; i<collection.length; i++){
+// Function to display the collection
+function showCollection(collection) {
+  for (let i = 0; i < collection.length; i++) {
     console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
   }
 }
 
+// Display the collection
 console.log("Showing my collection:");
 showCollection(myCollection);
 
-function findByArtist(collection, artistToFind){
+// Function to find albums by a specific artist
+function findByArtist(collection, artistToFind) {
   let results = [];
-  for(let i=0; i<collection.length; i++){
-    if(collection[i].artist == artistToFind){
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].artist == artistToFind) {
       results.push(collection[i]);
     }
   }
   return results;
 }
 
+// Search for albums by specific artists
 console.log("Searching for albums by Mitski, expect 1 result:", findByArtist(myCollection, 'Mitski'));
 console.log("Searching for albums by Weyes Blood, expect 1 result:", findByArtist(myCollection, 'Weyes Blood'));
 console.log("Searching for albums by Lykke Li, expect 1 result:", findByArtist(myCollection, 'Lykke Li'));
 console.log("Searching for albums by Chairlift, expect 1 result:", findByArtist(myCollection, 'Chairlift'));
 console.log("Searching for albums by Pixies, expect 0 results:", findByArtist(myCollection, 'Pixies'));
-
-
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
